71093d6aa1500655b9407e4ea5654a3133737948
kim hyeonmin <futurely08@naver.com>
2023-09-10 19:12:24 +0900

    계산기 웹 프로그램 구현하기(Thread Pool 적용)

--- was-practice/src/main/java/org/example/calculator/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/calculator/CustomWebApplicationServer.java
@@ -1,18 +1,19 @@
 package org.example.calculator;
 
-import org.example.calculator.domain.Calculator;
-import org.example.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
     public CustomWebApplicationServer(int port) {
@@ -29,8 +30,7 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null){
                 logger.info("[CustomWebApplicationServer] client connected!");
 
-                new Thread(new ClientRequestHandler(clientSocket)).start();
-
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
